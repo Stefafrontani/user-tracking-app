1b89233d52784e537c88aa8d5695e18bd4083cf1
Stefano Frontani <stefano.frontani@globant.com>
2020-06-07 21:18:18 -0300

    Layout structure creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,303 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="content">
+      <header>
+        <div className="logo-container">
+          <img src={logo} alt="logo" className="lazy-loaded" />
+        </div>
+        <div className="content__personal-info-container">
+          <p>Conocimientos</p>
+          <ul>
+            <li>Nombre</li>
+            <li>Edad</li>
+            <li>Mail</li>
+            <li>Telefono</li>
+          </ul>
+        </div>
+        <div className="content__personal-info-container">
+          <p>Conocimientos</p>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>Angular</li>
+          </ul>
+        </div>
+        <div className="content__personal-info-container">
+          <p>Conocimientos</p>
+          <ul>
+            <li>Cantar</li>
+            <li>Comer</li>
+            <li>Guitarra</li>
+            <li>Cocinar</li>
+          </ul>
+        </div>
       </header>
+      <p className="content__text">
+        Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto
+        Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto
+        Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto
+        Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto
+        Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto Texto
+        Texto Texto Texto Texto Texto Texto Texto Texto{" "}
+      </p>
+      <div className="content__video-container">
+        <div className="content__video">
+          <video width="100%" height="100%" poster={logo} controls>
+            <source src="lanona.mp4" type="video/ogg" />
+          </video>
+          No video for you, sorry not sorry
+        </div>
+      </div>
+      <div className="content__images-container">
+        <div className="content__image">
+          <img alt="description 1" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 2" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 3" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+        <div className="content__image">
+          <img alt="description 4" className="lazy-loaded" src={logo}></img>
+        </div>
+      </div>
     </div>
   );
 }
